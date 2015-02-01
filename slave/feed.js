3e538b82e127977152860575352dbeefa6d1402e
f00gasi <b.jarkowicz@gmail.com>
2015-02-01 23:08:19 +0100

    beta

--- slave/feed.js
+++ slave/feed.js
@@ -21,18 +21,18 @@
 			$.each(data, function(i,value){
 
 				$.each(value, function(i,obj){
-					//console.log(this);
 					if(obj.hasOwnProperty('message')||obj.hasOwnProperty('picture')){
-						console.log(this);
+						//console.log(this);
 						var postURL ='http://www.facebook.com/'+userdataobj.username+'/posts/';
 						var postID = this.id.split('_')[1];
 						var $postDIV = $('<div class="post-content" />');
-						var $msgDiv = $('<div />').addClass('content-inner');
+						var $msgDiv = $('<div class="content-inner" />');
+						var $socialDiv = $('<div class="social" />');
 						postURL += postID;
 
 						// MESSAGE
 						if(this.message !== undefined){
-							var msgtext = this.message;
+							var msgtext = shortenText(this.message);
 							post = $('<p>')
 									.addClass('message')
 									.html(msgtext.replace(/\n/g , '<br />'))
@@ -84,15 +84,30 @@
 
 						// SOCIAL DATA
 						if(this.likes !== undefined){
-							socialdata = '<p>Likes: '+this.likes.data.length+'</p>';
+							socialdata = '<p><i class="fa fa-thumbs-up fa-1"></i> '+this.likes.data.length+'</p>';
 						}
 						if(this.comments !== undefined){
-							socialdata += '<p>Comments: '+this.comments.data.length+'</p>';
+							socialdata += '<p><i class="fa fa-comment fa-1"></i> '+this.comments.data.length+'</p>';
 						}
+						$socialDiv.append(socialdata);
 
-						$postDIV.append($dateDiv,$msgDiv,link,socialdata);
+						$postDIV.append($dateDiv,$msgDiv,link,$socialDiv);
 
 						container.append($postDIV);
+					}
+				});
+			});
+			$(document).ready(function(){
+				$('a.showmore').on('click',function(e){
+					e.preventDefault();
+					var hiddenSpan = $(this).siblings('span.hide');
+					if(hiddenSpan.css('display') == 'none'){
+						hiddenSpan.show();
+						$(this).html('... Weniger anzeigen');
+					}
+					else{
+						hiddenSpan.hide();
+						$(this).html('... Mehr anzeigen');
 					}
 				});
 			});
@@ -107,7 +122,6 @@
 
 		function createDiv(timestamp,link){
 			while(timestamp){
-				//var link = link.attr('href');
 				var div = $('<div />').addClass('post-meta');
 				var a = $('<a />').attr({'href':link,'target':'_blank'});
 				var datediv = $('<div />').addClass('date');
@@ -120,5 +134,19 @@
 				return div;
 			}
 		}
+
+		function shortenText(text){
+			var countchars = text.length;
+			var maxlength = Math.floor(countchars/2);
+			if(countchars > maxlength){
+				var cutText = text.substr(0, maxlength);
+				cutText = cutText.substr(0, Math.min(cutText.length, cutText.lastIndexOf(" ")));
+				var secondHalf = text.substr(cutText.length, text.lastIndexOf(" "));
+				var spanShow = '<span class="show">'+cutText+'</span>';
+				var spanHide = '<span class="hide">'+secondHalf+'</span>';
+				return spanShow+spanHide+' <a class="showmore" href="javascript:void(0)">... Mehr anzeigen</a>';
+			}
+		}
 	}
+
 })(jQuery);
