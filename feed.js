c0f7e854293d42b8760821dc8cada103632c8628
f00gasi <b.jarkowicz@gmail.com>
2015-02-10 00:05:32 +0100

    gamma

--- feed.js
+++ feed.js
@@ -1,32 +1,80 @@
 (function($) { 
-	$.fn.fbFeed = function (data,token){
+	$.fn.fbFeed = function (options){
+		var options = $.extend({
+			token : null,
+			feedID : null,
+			appID : null
+		}, options);
+
 		var container = $(this);
+		container.addClass('loading');
+		container.append('<div class="spinner"></div');
+
 		var post;
 		var img;
 		var link;
 		var socialdata;
 		var username;
+		var count=0;
 		var months = ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'];
-		FB.api(
-		    "/548395945199562?access_token="+token,
-		    function (getuserdata) {
-		    	if (getuserdata && !getuserdata.error) {
-		        	username = getuserdata;
-		        	init(username);
-		    	}
-		    }
-		);
 
-		function init(userdataobj){
+		// GET USER AND TRIGGER GETTING DATA FROM FACEBOOK
+		window.fbAsyncInit = function() {
+		  FB.init({
+		    appId      : options.appID,
+		    xfbml      : true,
+		    version    : 'v2.2'
+		  });
+			FB.api(
+			    options.feedID+"?access_token="+options.token,
+			    function (userdata) {
+			    	if (userdata && !userdata.error) {
+			        	username = userdata.username;
+			        	getFeed(options.feedID,options.token,username);
+			    	}
+			    }
+			);
+		};
+
+		function init(user,data){
 			$.each(data, function(i,value){
-
 				$.each(value, function(i,obj){
-					//console.log(this);
 					if(obj.hasOwnProperty('message')||obj.hasOwnProperty('picture')){
-						var postURL ='http://www.facebook.com/'+userdataobj.username+'/posts/';
+						var postURL ='http://www.facebook.com/'+user+'/posts/';
 						var postID = this.id.split('_')[1];
-						var postDIV = $('<div class="fbPost" />');
+						var $postDIV = $('<div class="post-content" />');
+						var $msgDiv = $('<div class="content-inner" />');
+						var $socialDiv = $('<div class="social" />');
 						postURL += postID;
+
+						// MESSAGE
+						if(this.message !== undefined){
+							var msgtext = shortenText(this.message);
+							post = $('<p>')
+									.addClass('message')
+									.html(msgtext.replace(/\n/g , '<br />'))
+									.css('margin-bottom' , '20px');
+							$msgDiv.append(post);
+						}
+						else {
+							post = $('<p>')
+									.addClass('message')
+									.css('margin-bottom' , '20px')
+									.html('Neues Foto hinzugefügt');
+							$msgDiv.append(post);
+						}
+
+						// LINK
+						if(this.link !== undefined){
+							link = $('<a />')
+									.attr({'href':this.link,'target':'_blank'})
+									.html('Beitrag ansehen');
+						}
+						else{
+							link = $('<a />')
+									.attr({'href':postURL,'target':'_blank'})
+									.html('Beitrag ansehen');
+						}
 
 						// DATE OF POST
 						if(this.created_time !== undefined){
@@ -34,31 +82,7 @@
 							var datestr = timestamp.substr(0,timestamp.indexOf('T'));
 							var time = timestamp.substr(timestamp.indexOf('T')+1,5)
 							datestr = convertDate(datestr);
-							var $dateDiv = createDiv(datestr+'|'+time);
-							console.log($dateDiv);
-						}
-
-						// MESSAGE
-						if(this.message !== undefined){
-							var msgtext = this.message;
-							post = $('<p>')
-									.addClass('message')
-									.html(msgtext.replace(/\n/g , '<br />'))
-									.css('margin-bottom' , '20px');
-						}
-						else {
-							post = $('<p>')
-									.addClass('message')
-									.css('margin-bottom' , '20px')
-									.html('Neues Foto hinzugefügt');
-						}
-
-						// LINK
-						if(this.link !== undefined){
-							link = '<br><a href="'+this.link+'" target="_blank">Beitrag ansehen</a>';
-						}
-						else{
-							link = '<br><a href="'+postURL+'" target="_blank">Beitrag ansehen</a>';
+							var $dateDiv = createDiv(datestr+'|'+time,link.attr('href'));
 						}
 
 						// PICTURE
@@ -69,6 +93,7 @@
 							});
 							img = $('<img>').attr('src' , this.picture);
 							img.appendTo(imglink);
+							$msgDiv.append(imglink);
 						}
 						else{
 							img = '';
@@ -76,18 +101,46 @@
 
 						// SOCIAL DATA
 						if(this.likes !== undefined){
-							socialdata = '<p>Likes: '+this.likes.data.length+'</p>';
+							socialdata = '<p><i class="fa fa-thumbs-up fa-1"></i> '+this.likes.data.length+'</p>';
 						}
 						if(this.comments !== undefined){
-							socialdata += '<p>Comments: '+this.comments.data.length+'</p>';
+							socialdata += '<p><i class="fa fa-comment fa-1"></i> '+this.comments.data.length+'</p>';
 						}
+						$socialDiv.append(socialdata);
 
-						postDIV.append($dateDiv,post,imglink,link,socialdata,'<hr />');
+						$postDIV.append($dateDiv,$msgDiv,link,$socialDiv);
 
-						container.append(postDIV);
+						container.append($postDIV);
 					}
 				});
 			});
+			$('a.showmore').on('click',function(e){
+				e.preventDefault();
+				var hiddenSpan = $(this).siblings('span.hide');
+				if(hiddenSpan.css('display') == 'none'){
+					hiddenSpan.show();
+					$(this).html('... Weniger anzeigen');
+				}
+				else{
+					hiddenSpan.hide();
+					$(this).html('... Mehr anzeigen');
+				}
+			});
+		}
+
+		function getFeed(id,token,user){
+		  FB.api(
+		        id+'/feed?access_token='+token+'&limit=20', function (response) {
+		        if (response && !response.error) {
+		        	init(user,response);
+					setTimeout(function(){
+						container.removeClass('loading');
+					},350);	
+		        }
+		        if(response.error){
+		          console.log(response.error);
+		        }
+		  });
 		}
 
 		function convertDate(date){
@@ -97,15 +150,36 @@
 			return split[2]+' '+m;
 		}
 
-		function createDiv(timestamp){
+		function createDiv(timestamp,link){
 			while(timestamp){
-				var div = $('<div />').addClass('date');
+				var div = $('<div />').addClass('post-meta');
+				var a = $('<a />').attr({'href':link,'target':'_blank'});
+				var datediv = $('<div />').addClass('date');
 				var split = timestamp.split('|');
 				var span1 = $('<span />').addClass('daymonth').html(split[0]);
 				var span2 = $('<span />').addClass('time').html(split[1]);
-				div.append(span1,span2);
+				datediv.append(span1,span2);
+				a.append(datediv);
+				div.append(a);
 				return div;
 			}
 		}
+		
+		var maxlength = 165;
+		function shortenText(text){
+			var countchars = text.length;
+			if(countchars > maxlength){
+				var cutText = text.substr(0, maxlength);
+				cutText = cutText.substr(0, Math.min(cutText.length, cutText.lastIndexOf(" ")));
+				var secondHalf = text.substr(cutText.length, text.lastIndexOf(" "));
+				var spanShow = '<span class="show">'+cutText+'</span>';
+				var spanHide = '<span class="hide">'+secondHalf+'</span>';
+				return spanShow+spanHide+' <a class="showmore" href="javascript:void(0)">... Mehr anzeigen</a>';
+			}
+			else{
+				return text;
+			}
+		}
 	}
+
 })(jQuery);
